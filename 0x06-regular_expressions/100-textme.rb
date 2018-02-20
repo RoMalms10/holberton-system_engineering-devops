d39b29bc48e90fae6b3134f6fa719da533a91836
RoMalms10 <156@holbertonschool.com>
2018-02-20 22:49:19 +0000

    Change from 10 digit number to a +

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
-print ARGV[0].scan(/from:(\+?[0-9]{10}|[a-zA-Z]+)/).join
+print ARGV[0].scan(/from:(\+?[0-9]+|[a-zA-Z]+)/).join
 print(",")
-print ARGV[0].scan(/to:(\+?[0-9]{10}|[a-zA-Z]+)/).join
+print ARGV[0].scan(/to:(\+?[0-9]+|[a-zA-Z]+)/).join
 print(",")
 print ARGV[0].scan(/flags:(\-?[0-9]\:\-?[0-9]\:\-?[0-9]\:\-?[0-9]\:\-?[0-9])/).join
 print("\n")
