35a20a2f359f33c54426fca22097ccb9a637b293
RoMalms10 <156@holbertonschool.com>
2018-02-20 22:46:40 +0000

    Add parenthesis

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -3,5 +3,5 @@ print ARGV[0].scan(/from:(\+?[0-9]{10}|[a-zA-Z]+)/).join
 print(",")
 print ARGV[0].scan(/to:(\+?[0-9]{10}|[a-zA-Z]+)/).join
 print(",")
-print ARGV[0].scan(/\-?[0-9]\:\-?[0-9]\:\-?[0-9]\:\-?[0-9]\:\-?[0-9]/).join
+print ARGV[0].scan(/flags:(\-?[0-9]\:\-?[0-9]\:\-?[0-9]\:\-?[0-9]\:\-?[0-9])/).join
 print("\n")
