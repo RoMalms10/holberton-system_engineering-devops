6d0e7841f3f543590ccbcb2e742e7ededd2c45f0
Robert MALMSTEIN <156@p2-4.hbtn.io>
2018-04-30 14:20:00 -0700

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' This module is used for getting information from an API '''
+import requests
+from sys import argv
+
+count = 0
+true_dict = {}
+response1 = requests.get(
+    'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
+info = response1.json()
+response2 = requests.get(
+    'https://jsonplaceholder.typicode.com/todos?userId={}'.format(argv[1]))
+tasks = response2.json()
+for obj in tasks:
+    if obj.get('completed') is True:
+        true_dict['title{}'.format(count)] = obj.get('title')
+        count += 1
+print('Employee {} is done with tasks({}/{}):'.format(
+    info.get('name'), count, len(tasks)))
+for key, value in true_dict.items():
+    print('\t {}'.format(value))
