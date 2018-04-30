ea6059cc8d8d3c45861a4ee7c4d35918adafaf44
romalms10 <romalms10@gmail.com>
2018-04-30 23:11:42 +0000

    Grab all information from API and turn into JSON

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+''' This module is used for getting information from an API '''
+import json
+import requests
+
+if __name__ == "__main__":
+    response1 = requests.get('https://jsonplaceholder.typicode.com/users')
+    info = response1.json()
+    response2 = requests.get('https://jsonplaceholder.typicode.com/todos')
+    tasks = response2.json()
+    json_dict = {}
+    task_list= []
+    index = 0
+    for user in info:
+        userid = user.get('id')
+        for idx in range(index, len(tasks)):
+            if tasks[idx].get('userId') == userid:
+                tasks[idx].pop('id', None)
+                tasks[idx].pop('userId', None)
+                tasks[idx]['username'] = user.get('username')
+                task_list.append(tasks[idx])
+            else:
+                index = idx
+                json_dict[userid] = task_list
+                task_list = []
+                break
+    with open('todo_all_employees.json', 'w') as jsonfile:
+        json.dump(json_dict, jsonfile, sort_keys=True)
