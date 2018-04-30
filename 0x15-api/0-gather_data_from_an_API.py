8d1aa71b97e2d5915fa32ce9191837ed992663fe
romalms10 <romalms10@gmail.com>
2018-04-30 23:05:55 +0000

    change dict to list

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,7 +5,7 @@ from sys import argv
 
 if __name__ == "__main__":
     count = 0
-    true_dict = {}
+    true_list = []
     response1 = requests.get(
         'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1]))
     info = response1.json()
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     tasks = response2.json()
     for obj in tasks:
         if obj.get('completed') is True:
-            true_dict['title{}'.format(count)] = obj.get('title')
+            true_list.append(obj.get('title'))
             count += 1
     print('Employee {} is done with tasks({}/{}):'.format(
         info.get('name'), count, len(tasks)))
-    for key, value in true_dict.items():
-        print('\t {}'.format(value))
+    for string in true_list:
+        print('\t {}'.format(string))
